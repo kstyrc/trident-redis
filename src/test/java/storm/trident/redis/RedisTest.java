56c02fb872a9a7f9c20e1ee2eae29eeb046d924a
kstyrc <kstyrc@gmail.com>
2014-07-09 13:07:51 +0200

    Refactored code. Fixed try..finally on jedis resource.

--- src/test/java/storm/trident/redis/RedisTest.java
+++ src/test/java/storm/trident/redis/RedisTest.java
@@ -1,88 +1,115 @@
 package storm.trident.redis;
 
-import static org.junit.Assert.assertEquals;
-
-import java.net.InetSocketAddress;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
-
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 import redis.embedded.RedisServer;
 import storm.trident.state.StateFactory;
 import storm.trident.state.map.MapState;
 
-import com.google.common.collect.Lists;
-import com.google.common.io.Resources;
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
 
 public class RedisTest {
-	private static final long SEED = 8682522807148012L;
-	private static final Random RANDOM = new Random(SEED);
-	
-	private static final String HOSTNAME = "localhost";
-	private static final int PORT = 6379;
-	
-	private RedisServer redisServer;
+   private static final long SEED = 8682522807148012L;
+   private static final Random RANDOM = new Random(SEED);
 
-	
-	@Before
-	public void setup() throws Exception {
-        redisServer = new RedisServer(PORT);
-        redisServer.start();
-        cleanup();
-	}
-	
-	@Test
-	public void testCache() {
-		try {
-			StateFactory redis = RedisState.nonTransactional(new InetSocketAddress(HOSTNAME, PORT));
-			MapState state = (MapState) redis.makeState(new HashMap(), null, 0, 0);
-	
-			// insert some key-value pairs
-			String[] keys = new String[]{"foo", "bar", "baz"};
-		    List<Integer> vals = Lists.newArrayList();
-		    List<List<Object>> keyList = Lists.newArrayList();
-		    for (String key : keys) {
-		        List<Object> l = Lists.newArrayList();
-		        l.add(key);
-		        keyList.add(l);
-		        vals.add(RANDOM.nextInt());
-		    }
-		    state.multiPut(keyList, vals);
-		    
-			// Verify the retrieval of the kv pairs.
-			List<Integer> actualVals = state.multiGet(keyList);
-			assertEquals(vals.size(), actualVals.size());
-			for (int i = 0; i < vals.size(); i++) {
-				assertEquals(vals.get(i), actualVals.get(i));
-			}
-		
-		} catch (JedisConnectionException e) {
-			throw new RuntimeException(String.format("Unfortunately, Jedis is unable to connect to redis server [%s:%i]", HOSTNAME, PORT), e);
-		}
-	}
-	
-	@After
-	public void tearDown() {
-		cleanup();
-        redisServer.stop();
-	}
-	
-	private void cleanup() {
-		try {
-			Jedis jedis = new Jedis(HOSTNAME, PORT);
-			jedis.flushAll();
-			jedis.disconnect();
-			
-		} catch (JedisConnectionException e) {
-			throw new RuntimeException(String.format("Unfortunately, Jedis is unable to connect to redis server [%s:%i]", HOSTNAME, PORT), e);
-		}
-	}
+   private static final String HOSTNAME = "localhost";
+   private static final int PORT = 6379;
+
+   private RedisServer redisServer;
+
+
+   @Before
+   public void setup() throws Exception {
+      redisServer = new RedisServer(PORT);
+      redisServer.start();
+      cleanup();
+   }
+
+   @Test
+   public void testCache() {
+      RedisState.Options<Object> opts = new RedisState.Options<Object>();
+      opts.localCacheSize = 0;
+      StateFactory redis = RedisState.nonTransactional(new InetSocketAddress(HOSTNAME, PORT), opts);
+      MapState state = (MapState) redis.makeState(new HashMap(), null, 0, 0);
+
+      List<List<Object>> keys = Arrays.asList(
+            Arrays.<Object>asList("foo"),
+            Arrays.<Object>asList("bar"),
+            Arrays.<Object>asList("bazz")
+      );
+      List<Integer> values = Arrays.asList(
+            RANDOM.nextInt(), RANDOM.nextInt(), RANDOM.nextInt()
+      );
+      insertKeyValues(state, keys, values);
+      verifyKeyValues(state, keys, values);
+   }
+
+   @Test
+   public void testCacheWithCustomKeyFactory() throws Exception {
+      RedisState.Options<Object> opts = new RedisState.Options<Object>();
+      opts.localCacheSize = 1;
+      StateFactory redis = RedisState.nonTransactional(
+            new InetSocketAddress(HOSTNAME, PORT),
+            opts,
+            new RedisState.KeyFactory() {
+               @Override
+               public String build(List<Object> key) {
+                  if (key.size() >= 2) {
+                     return key.get(0).toString() + key.get(1).toString();
+                  } else {
+                     return key.get(0).toString();
+                  }
+               }
+            });
+      MapState state = (MapState) redis.makeState(new HashMap(), null, 0, 0);
+
+      List<List<Object>> keys = Arrays.asList(
+            Arrays.<Object>asList("foo", "1", "2"),
+            Arrays.<Object>asList("foo", "2", "3"),
+            Arrays.<Object>asList("bazz", "1", "2")
+      );
+      List<Integer> values = Arrays.asList(
+            RANDOM.nextInt(), RANDOM.nextInt(), RANDOM.nextInt()
+      );
+      insertKeyValues(state, keys, values);
+      verifyKeyValues(state, keys, values);
+   }
+
+   private void insertKeyValues(MapState state, List<List<Object>> keys, List<Integer> values) {
+      state.multiPut(keys, values);
+   }
+
+   private void verifyKeyValues(MapState state, List<List<Object>> keys, List<Integer> values) {
+      List<?> actualValues = state.multiGet(keys);
+      assertEquals(values.size(), actualValues.size());
+      for (int i = 0; i < values.size(); i++) {
+         assertEquals(values.get(i).toString(), actualValues.get(i).toString());
+      }
+   }
+
+   @After
+   public void tearDown() {
+      cleanup();
+      redisServer.stop();
+   }
+
+   private void cleanup() {
+      try {
+         Jedis jedis = new Jedis(HOSTNAME, PORT);
+         jedis.flushAll();
+         jedis.disconnect();
+
+      } catch (JedisConnectionException e) {
+         throw new RuntimeException(String.format("Unfortunately, Jedis is unable to connect to redis server [%s:%i]", HOSTNAME, PORT), e);
+      }
+   }
 }
